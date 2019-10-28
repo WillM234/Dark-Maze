6f76eda1cad9c1e4cdfa9b61708c94adb5edd529
Will M. 3 <willmcb234@gmail.com>
2019-10-28 12:47:23 -0400

    Level1.1_Added

--- Project1.Maze/Assets/Scripts/PlayerScripts/Player_Script.cs
+++ Project1.Maze/Assets/Scripts/PlayerScripts/Player_Script.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player_Script : MonoBehaviour
 {
+private float speed = .005f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,21 @@ public class Player_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+if(Input.GetKey(KeyCode.A))
+    {
+    transform.Translate(Vector2.left*speed); 
+    }
+if(Input.GetKey(KeyCode.D))
+    {
+    transform.Translate(Vector2.right*speed);
+    }
+if(Input.GetKey(KeyCode.W))
+    {
+    transform.Translate(Vector2.up*speed);
+    }
+if(Input.GetKey(KeyCode.S))
+    {
+    transform.Translate(Vector2.down*speed);
+    }
     }
 }
