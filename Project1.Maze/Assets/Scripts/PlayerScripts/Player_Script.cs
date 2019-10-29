23f4e06a063b2cdaed61bce3c35944355fbe4747
Will M <willmcb234@gmail.com>
2019-10-29 13:04:01 -0400

    EnemyMovement_Fixed_Player_Movement

--- Project1.Maze/Assets/Scripts/PlayerScripts/Player_Script.cs
+++ Project1.Maze/Assets/Scripts/PlayerScripts/Player_Script.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class Player_Script : MonoBehaviour
 {
 private float speed = .005f;
+private Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
 if(Input.GetKey(KeyCode.A))
-    {
-    transform.Translate(Vector2.left*speed); 
+        {
+            transform.Translate(Vector2.left * speed);
+   /*rb2d.AddForce(-transform.right * speed * Time.deltaTime)*/; 
     }
 if(Input.GetKey(KeyCode.D))
-    {
-    transform.Translate(Vector2.right*speed);
+        {
+            transform.Translate(Vector2.right * speed);
+    /*rb2d.AddForce(transform.right * speed * Time.deltaTime)*/;
     }
 if(Input.GetKey(KeyCode.W))
-    {
-    transform.Translate(Vector2.up*speed);
+        {
+            transform.Translate(Vector2.up * speed);
+    /*rb2d.AddForce(transform.forward*speed * Time.deltaTime)*/;
     }
 if(Input.GetKey(KeyCode.S))
-    {
-    transform.Translate(Vector2.down*speed);
+        {
+            transform.Translate(Vector2.down * speed);
+    /*rb2d.AddForce(-transform.forward*speed * Time.deltaTime)*/;
     }
     }
 }
