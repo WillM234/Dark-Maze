56b2b3c56f41693637cf5160e7d02f093eb4abd0
Will M. 3 <willmcb234@gmail.com>
2019-10-29 15:49:19 -0400

    Starting_Light_Stuff

---
+++ Project1.Maze/Assets/Scripts/EnemyScripts/EnemyScriptYmove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScriptYmove : MonoBehaviour
+{
+public float upMax;
+public float downMax;
+public float speed;
+
+private Vector2 MVect;
+    // Start is called before the first frame update
+    void Start()
+    {
+MVect.y = -1f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+Vector2 newPos = new Vector2(transform.position.x + MVect.x * speed, transform.position.y + MVect.y * speed); 
+
+//constrains enemy movemnet. Has to be set in editor
+    if(newPos.y <= upMax)
+        {
+        speed = -speed;
+        }
+    if(newPos.y >= downMax)
+        {
+        speed = -speed;           
+        }
+transform.position = newPos;
+    }
+}
---
+++ Project1.Maze/Assets/Scripts/EnvironmentScripts/LightControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControler : MonoBehaviour
+{
+public List<Transform> Lights; 
+public float Position; 
+public float Strength;
+public float Falloff; 
+
+private void Update()
+{
+for (int i = 0; i < Lights.Count; i++)
+    {
+    Lights[i].position = Vector2.Lerp(transform.position, Position, Strength * (i Falloff));
+    }
+}
+}
